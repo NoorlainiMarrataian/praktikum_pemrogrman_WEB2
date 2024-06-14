1d3b045291aff46d4db8eb524c861fb4a63913ee
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2024-06-14 16:27:11 +0800

    Add files via upload

--- Modul 1/PRAK101.php
+++ Modul 1/PRAK101.php
@@ -1,1 +1,17 @@
-
+<?php
+$nama = "Noorlaini Marratain";
+$nim = "2210817120012";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum Soal 1 Modul 1</title>
+</head>
+<body>
+    <p>Hello World in PHP</p>
+    <p>Nama Praktikan: <?php echo $nama; ?></p>
+    <p>NIM: <?php echo $nim; ?></p>
+</body>
+</html>
